d0341ebac68d5afba00527e4c84fd3e2027fe561
Divinekeyz123 <christopherdivine84@gmail.com>
2025-03-27 03:19:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "2347053525276"
+global.ownername = "PIE"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
@@ -22,13 +22,13 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 
 
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 
 //sticker details
